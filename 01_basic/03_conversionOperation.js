fa5c3be71dd42fac2043d87d5d77641d6ef1e0ab
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-03-02 16:33:25 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,50 @@
+//let score = true
+//console.log(typeof score);
+
+//let valueintNumber = Number(score)
+//console.log(typeof valueintNumber);
+//console.log(valueintNumber);
+
+
+//"33" = 33
+//"33abc" => NaN
+//true=>1 , false=>0
+
+//let isloggedIn = ""
+//let booleanisLoggedIn= Boolean (isloggedIn)
+//console.log(booleanisLoggedIn);
+
+//let isloggedin= ""
+//let BooleanisLoggedIn = Boolean(isloggedin)
+//console.log(booleanisLoggedIn);
+
+// let someNumber = 33
+//  let StringNumber = String(someNumber)
+//  console.log(StringNumber);
+//  console.log(typeof StringNumber);
+
+ //****************************** Operations **************************** *//
+
+ let value = -2.3
+ let negvalue = 1003 * value
+ console.log(negvalue);
+
+ let string1 = "antima"
+ let string2 = "punnu" 
+ let concatstring = string1 + string2
+ console.log(concatstring);
+
+ console.log("1" + 2+ 2);
+ console.log(2+2+"1");
+ console.log(3 + (3*4) + 8 % 7 );
+
+ let gamecounter=110;
+ --gamecounter;
+ console.log(gamecounter);
+
+ console.log("2" > 3);
+ console.log("02" < 1);
+ //reason for the operators called ==, >=,<=,<< where the == sign should work differently whereas ><= works differently 
+ //comparison convert null to numbers treating it as a 0 that why null >= 0 is true and null> 0 is false
+ 
+ 
