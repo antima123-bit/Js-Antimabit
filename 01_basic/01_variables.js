b8eaa52a02da56bdc277ee19cc8d275409b93087
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-03-02 16:32:56 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1444553
+let accountEmail = "antimamishra113@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rewa"
+let accountStat;
+
+//accountId = 222//not allowed because it is a constant never be change
+
+console.log(accountId);
+
+
+
+
+
+accountEmail="antima@gmail.com"
+accountCity="delhi"
+accountPassword="12345"
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountStat])
