c1e962f2fd00b0f030f3870439ccc514534961da
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-03-02 16:33:10 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //use java code in newwer version js
+
+//alert( 3+ 3)// weare use njs not broswer
+
+console.log(3+3); //that good readbility
+ 
+console.log("hitesh");
+let name="antima"
+let age=19
+let isloggedIn = true
+let state = null
+
+console.log(typeof age);
+//bigint is use when the number are very large in nature
+//null=>standalone values
+//undefined=>always undefined
